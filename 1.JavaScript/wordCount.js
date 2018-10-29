511230a47452dc6f2ef9e2e93808486b9d27d8bc
Zach Zendejas <ZAX1527@gmail.com>
2018-10-29 17:09:53 -0600

    Correction

---
+++ 1.JavaScript/wordCount.js
@@ -0,0 +1,13 @@
+function myFunction(userInput) {
+   // verify that function is working properly.
+   console.log("Function called");
+   console.log(userInput);
+
+   // converts userInput object to userArray array
+   var userArray = Object.values(userInput);
+   console.log(userArray);
+
+   // place userArray into words
+
+   console.log(words);
+}
